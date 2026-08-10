1d3f0081d1e928869652933765eadda6dec1e25e
RyanGosling-AI <damnedwretchofficial@gmail.com>
2026-08-10 17:19:16 +1000

    Запуск автотестов на разных браузерах

--- Fixtures/browsers.py
+++ Fixtures/browsers.py
@@ -1,25 +1,29 @@
 import pytest
-import allure
 from playwright.sync_api import sync_playwright, Page, Playwright
 from _pytest.fixtures import SubRequest
 from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialize_playwright_page
 from config import settings
+from tools.routes import AppRoute
 
 
-@pytest.fixture
-def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+@pytest.fixture(params=settings.browsers)
+def page(request: SubRequest, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        browser_type=request.param
+    )
 
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=settings.headless)
     context = browser.new_context(base_url=settings.get_base_url())
     page = context.new_page()
 
     registration_page = RegistrationPage(page=page)
-    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.visit(AppRoute.REGISTRATION)
     registration_page.registration_form.fill(
         email=settings.test_user.email,
         username=settings.test_user.username,
@@ -31,10 +35,11 @@ def initialize_browser_state(playwright: Playwright) -> Page:
     browser.close()
 
 
-@pytest.fixture(scope="function")
-def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
+@pytest.fixture(scope="function", params=settings.browsers)
+def page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
+        browser_type=request.param,
         storage_state=settings.browser_state_file
     )
